91ea2d68a92d878e04aa93b17cab9cee70186da1
Micah Seneshen <micah.seneshen@blizzardbird.com>
2017-10-20 17:38:27 -0600

    Added Android Studio project skeleton

---
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/MainActivity.java
@@ -0,0 +1,13 @@
+package ca.ualberta.cmput301f17t08.habitrabbit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
