2fd27c2eb19a10277e1ff16d1416b974b1a5f01f
Micah Seneshen <mseneshen@gmail.com>
2017-10-22 18:50:05 -0600

    Added map unit test

---
+++ app/src/test/java/ca/ualberta/cmput301f17t08/habitrabbit/MapUnitTest.java
@@ -0,0 +1,69 @@
+package ca.ualberta.cmput301f17t08.habitrabbit;
+
+import android.location.Location;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.File;
+
+import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.fail;
+
+public class MapUnitTest {
+
+    Location location1, location2;
+    Map map;
+
+    @Before
+    public void setUp() {
+
+        location1 = new Location("");
+        location2 = new Location("");
+
+        location1.setLatitude(10);
+        location1.setLongitude(10);
+
+        location2.setLatitude(20);
+        location2.setLongitude(20);
+
+        map = new Map();
+    }
+
+    @Test
+    public void testMapAddLocation() throws Exception {
+        assertFalse(map.getLocations().contains(location1));
+        assertFalse(map.getLocations().contains(location2));
+
+        map.addLocation(location1);
+
+        assertTrue(map.getLocations().contains(location1));
+        assertFalse(map.getLocations().contains(location2));
+
+        map.addLocation(location2);
+
+        assertTrue(map.getLocations().contains(location1));
+        assertTrue(map.getLocations().contains(location2));
+    }
+
+    @Test
+    public void testMapRemoveLocation() throws Exception {
+
+        map.addLocation(location1);
+        map.addLocation(location2);
+
+        assertTrue(map.getLocations().contains(location1));
+        assertTrue(map.getLocations().contains(location2));
+
+        map.removeLocation(location1);
+
+        assertFalse(map.getLocations().contains(location1));
+        assertTrue(map.getLocations().contains(location2));
+
+        map.removeLocation(location2);
+
+        assertFalse(map.getLocations().contains(location1));
+        assertFalse(map.getLocations().contains(location2));
+    }
+
+}
