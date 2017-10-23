fca3011d6274121c27021a45591850d89b189626
Micah Seneshen <mseneshen@gmail.com>
2017-10-23 15:31:36 -0600

    Added User skeleton

---
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/User.java
@@ -0,0 +1,19 @@
+package ca.ualberta.cmput301f17t08.habitrabbit;
+
+import java.util.ArrayList;
+
+/**
+ * Created by mseneshen on 2017-10-23.
+ */
+
+public class User {
+
+    public ArrayList<User> getFollowers() { return null; }
+    public void addFollower(User follower) { return; }
+    public void removeFollower(User follower) { return; }
+
+    public ArrayList<Habit> getHabits() { return null; }
+    public void addHabit(Habit habit) { return; }
+    public void removeHabit(Habit habit) { return; }
+
+}
