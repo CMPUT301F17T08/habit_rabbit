41b6525e3f0143196f92a8bf3acaa72c3605df10
Maharsh Patel <patel@maharsh.net>
2017-10-23 11:18:42 -0600

    habit class empty functions

---
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/Habit.java
@@ -0,0 +1,49 @@
+package ca.ualberta.cmput301f17t08.habitrabbit;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by maharshmellow on 2017-10-23.
+ */
+
+public class Habit {
+    public Habit(String name, String reason, Date date, int[] frequency){
+        return;
+    }
+
+    public void setName(String name){
+        return;
+    }
+
+    public void setReason(String reason){
+        return;
+    }
+
+    public void setDate(Date date){
+        return;
+    }
+
+    public void setFrequency(int[] frequency){
+        return;
+    }
+
+    // TODO need to change the return types for these functions when we have proper return data
+    public void getName(){
+        return;
+    }
+
+    public void getReason(){
+        return;
+    }
+
+    public void getDate(){
+        return;
+    }
+
+    public void getFrequency(){
+        return;
+    }
+    
+
+}
