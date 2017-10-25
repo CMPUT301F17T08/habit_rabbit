8358cb0c5372b3de09cac97d4730e4c5c4e60215
Maharsh Patel <patel@maharsh.net>
2017-10-24 18:57:41 -0600

    added some functions - WIP

--- app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/Habit.java
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/Habit.java
@@ -8,39 +8,68 @@ import java.util.List;
  */
 
 public class Habit {
-    public Habit(String name, String reason, Date date, int[] frequency){
-        return;
+    String name;
+    String reason;
+    Date startDate;
+    Date lastCompleted;
+    int[] frequency;
+    int daysCompleted;
+    int averageTime;
+    int streak;
+
+    public Habit(String name, String reason, Date startDate, int[] frequency){
+        this.name = name;
+        this.reason = reason;
+        this.startDate = startDate;
+        this.frequency = frequency;
+
+        this.lastCompleted = null;
+        this.daysCompleted = 0;
+        this.averageTime = 0;
+        this.streak = 0;
     }
 
     public void setName(String name){
-        return;
+        this.name = name;
     }
 
     public void setReason(String reason){
-        return;
+        this.reason = reason;
     }
 
-    public void setDate(Date date){
-        return;
+    public void setDate(Date startDate){
+        this.startDate = startDate;
     }
 
     public void setFrequency(int[] frequency){
-        return;
+        this.frequency = frequency;
     }
 
     public String getName(){
-        return null;
+        return this.name;
     }
 
     public String getReason(){
-        return null;
+        return this.reason;
     }
 
     public Date getDate(){
-        return null;
+        return this.startDate;
     }
 
     public int[] getFrequency(){
+        return this.frequency;
+    }
+
+
+    public int[] getStatistics(){
+
+        Date today = new Date();
+
+        int daysSinceStart = (int) Math.abs(today.getTime() - startDate.getTime())/(24 * 60 * 60 * 1000);
+        // returns days completed, streak, % completed in decimal, average time of completion
+        int[] statistics = {this.daysCompleted, this.streak, this.daysCompleted/daysSinceStart, this.averageTime};
+
         return null;
     }
 
