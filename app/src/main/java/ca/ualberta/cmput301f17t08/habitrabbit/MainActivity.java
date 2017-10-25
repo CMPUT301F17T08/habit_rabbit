3a93d9c78b95abf6ae0b3cf59c77de901f38eebc
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-10-24 23:03:52 -0600

    frequency done

--- app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/MainActivity.java
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/MainActivity.java
@@ -1,11 +1,7 @@
 package ca.ualberta.cmput301f17t08.habitrabbit;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
     private MainActivity activity = this;
@@ -13,30 +9,30 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.login);
-
-        final EditText usernameField = (EditText) findViewById(R.id.username_input_field);
-        Button loginButton = (Button) findViewById(R.id.login_button);
-        Button signupButton = (Button) findViewById(R.id.signup_button);
-
-        loginButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                // TODO handle login here
-                String username = usernameField.getText().toString();
-
-                System.out.println("Login Button Clicked - Username:" + username);
-            }
-        });
-
-        // small signup button below the login button
-        signupButton.setOnClickListener(new View.OnClickListener(){
-            @Override
-            public void onClick(View view){
-                Intent intent = new Intent(activity, SignupActivity.class);
-                startActivity(intent);
-            }
-        });
+        setContentView(R.layout.habit_item);
+//
+//        final EditText usernameField = (EditText) findViewById(R.id.username_input_field);
+//        Button loginButton = (Button) findViewById(R.id.login_button);
+//        Button signupButton = (Button) findViewById(R.id.signup_button);
+//
+//        loginButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                // TODO handle login here
+//                String username = usernameField.getText().toString();
+//
+//                System.out.println("Login Button Clicked - Username:" + username);
+//            }
+//        });
+//
+//        // small signup button below the login button
+//        signupButton.setOnClickListener(new View.OnClickListener(){
+//            @Override
+//            public void onClick(View view){
+//                Intent intent = new Intent(activity, SignupActivity.class);
+//                startActivity(intent);
+//            }
+//        });
 
     }
 }
