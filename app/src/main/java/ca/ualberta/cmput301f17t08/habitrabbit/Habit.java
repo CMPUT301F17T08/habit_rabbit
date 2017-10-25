a5e4442eec05ff83c62f691d32a1bf6c950c7969
Maharsh Patel <patel@maharsh.net>
2017-10-24 19:59:24 -0600

    statistics and incremental average time

--- app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/Habit.java
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/Habit.java
@@ -1,7 +1,10 @@
 package ca.ualberta.cmput301f17t08.habitrabbit;
 
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.jar.Pack200;
 
 /**
  * Created by maharshmellow on 2017-10-23.
@@ -14,7 +17,7 @@ public class Habit {
     Date lastCompleted;
     int[] frequency;
     int daysCompleted;
-    int averageTime;
+    long averageTime;        // average time of day in milliseconds
     int streak;
 
     public Habit(String name, String reason, Date startDate, int[] frequency){
@@ -25,7 +28,7 @@ public class Habit {
 
         this.lastCompleted = null;
         this.daysCompleted = 0;
-        this.averageTime = 0;
+        this.averageTime = -1;
         this.streak = 0;
     }
 
@@ -62,15 +65,44 @@ public class Habit {
     }
 
 
-    public int[] getStatistics(){
-
+    public List<Object> getStatistics(){
         Date today = new Date();
 
         int daysSinceStart = (int) Math.abs(today.getTime() - startDate.getTime())/(24 * 60 * 60 * 1000);
-        // returns days completed, streak, % completed in decimal, average time of completion
-        int[] statistics = {this.daysCompleted, this.streak, this.daysCompleted/daysSinceStart, this.averageTime};
+        // formats the average time
+        String averageTimeStr = new SimpleDateFormat("hh:mm a").format(new Date(this.averageTime));
 
-        return null;
+        List<Object> statistics = new ArrayList<Object>();
+        statistics.add(this.daysCompleted);
+        statistics.add(this.streak);
+        statistics.add(this.daysCompleted/daysSinceStart);  // % completed
+        statistics.add(averageTimeStr);
+
+        return statistics;
+    }
+
+    public void markDone(){
+        Date now = new Date();
+
+        this.daysCompleted += 1;
+
+        // update the average time of completion
+        if (this.averageTime != -1){
+            this.averageTime = averageTime + (1/this.daysCompleted)*(now.getTime() % 86400000 - averageTime);
+        }else{
+            this.averageTime = now.getTime() % 8640000;     // milliseconds elapsed until now today
+        }
+
+        // update the streak if the last update was yesterday
+        if (this.lastCompleted != null && now.getTime() - this.lastCompleted.getTime() > 86400000){
+            this.streak += 1;
+        }
+
+        this.lastCompleted = new Date();
+
+
+        // TODO create habit event here and jump to the add to habit history activity
+
     }
 
 
