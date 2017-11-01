dcea47f4a73ae27781bed92e0cb4d97dd26f14c7
Micah Seneshen <micah.seneshen@blizzardbird.com>
2017-11-01 17:37:29 -0600

    Finished initial habit list

---
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/MyHabitActivity.java
@@ -0,0 +1,38 @@
+package ca.ualberta.cmput301f17t08.habitrabbit;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+
+/**
+ * Created by micah on 01/11/17.
+ */
+
+public class MyHabitActivity extends AppCompatActivity {
+
+    private MyHabitActivity activity = this;
+
+    private ArrayList<Habit> habitList;
+    private HabitsAdapter cAdapt;
+    private RecyclerView habitsRecyclerView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.habit);
+
+        habitList = LoginManager.getInstance().getCurrentUser().getHabits();
+
+        habitsRecyclerView = (RecyclerView) findViewById(R.id.habit_recyclerview);
+        habitsRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
+
+        cAdapt = new HabitsAdapter(habitList);
+        habitsRecyclerView.setAdapter(cAdapt);
+
+    }
+
+}
