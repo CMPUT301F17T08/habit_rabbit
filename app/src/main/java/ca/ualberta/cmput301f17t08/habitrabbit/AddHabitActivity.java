8fa40bf94e4b02f01b1e375391bfb34d2bd312f4
Maharsh Patel <patel@maharsh.net>
2017-11-02 17:46:54 -0600

    habit error checking and date picker updates

--- app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/AddHabitActivity.java
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/AddHabitActivity.java
@@ -12,6 +12,7 @@ import android.widget.EditText;
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
 import java.util.Date;
+import java.util.TimeZone;
 
 public class AddHabitActivity extends AppCompatActivity {
     private AddHabitActivity activity = this;
@@ -20,14 +21,19 @@ public class AddHabitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_habit);
 
-        final EditText habitTitle = (EditText) findViewById(R.id.habit_title_field);
-        final EditText habitReason = (EditText) findViewById(R.id.habit_reason_field);
-        final EditText dateSelector= (EditText) findViewById(R.id.habit_date_selector);
-        Button addHabitButton = (Button) findViewById(R.id.add_habit_button);
-
+        final EditText habitTitle = findViewById(R.id.habit_title_field);
+        final EditText habitReason = findViewById(R.id.habit_reason_field);
+        final EditText dateSelector = findViewById(R.id.habit_date_selector);
+        Button addHabitButton = findViewById(R.id.add_habit_button);
 
         // Date Picker (Source: https://goo.gl/nmN56M)
-        final Calendar calendar = Calendar.getInstance();
+        final SimpleDateFormat format = new SimpleDateFormat("EEE, MMM d, yyyy");
+        final Calendar calendar = Calendar.getInstance(TimeZone.getTimeZone("America/Edmonton"));
+
+        // set the habit start date to the current date
+        dateSelector.setText(format.format(calendar.getTime()));
+
+        // date picker
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
@@ -38,18 +44,21 @@ public class AddHabitActivity extends AppCompatActivity {
                 calendar.set(Calendar.DAY_OF_MONTH, dayOfMonth);
 
                 // update the editText label
-                SimpleDateFormat format = new SimpleDateFormat("dd/MM/yy");
                 dateSelector.setText(format.format(calendar.getTime()));
             }
         };
-
+        // when the date field is clicked on the add habit page
         dateSelector.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 // TODO Auto-generated method stub
-                new DatePickerDialog(AddHabitActivity.this, R.style.date_picker, date, calendar
+                DatePickerDialog datePicker = new DatePickerDialog(AddHabitActivity.this, R.style.date_picker, date, calendar
                         .get(Calendar.YEAR), calendar.get(Calendar.MONTH),
-                        calendar.get(Calendar.DAY_OF_MONTH)).show();
+                        calendar.get(Calendar.DAY_OF_MONTH));
+
+                // ensure that a previous date can't be selected
+                datePicker.getDatePicker().setMinDate(System.currentTimeMillis());
+                datePicker.show();
             }
         });
 
@@ -61,7 +70,20 @@ public class AddHabitActivity extends AppCompatActivity {
                 String reason = habitReason.getText().toString();
                 Date d = calendar.getTime();
 
-                // TODO do error checking here
+                // input error checking
+                if (title.length() > 20){
+                    habitTitle.setError("Please keep title under 20 characters");
+                }
+                else if (title.length() == 0){
+                    habitTitle.setError("Please enter a title");
+                }
+
+                if (reason.length() > 30){
+                    habitReason.setError("Please keep reason under 30 characters");
+                }
+                else if (reason.length() == 0){
+                    habitReason.setError("Please enter a reason");
+                }
 
                 // TODO create a new habit object here and associate that with the user
             }
