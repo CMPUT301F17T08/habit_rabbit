b7fde4ea36bdb415709c8d8751297255cdc964e9
Maharsh Patel <patel@maharsh.net>
2017-11-01 20:56:26 -0600

    event listening for add button and minor fixes

--- app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/AddHabitActivity.java
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/AddHabitActivity.java
@@ -3,12 +3,15 @@ package ca.ualberta.cmput301f17t08.habitrabbit;
 import android.app.DatePickerDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 
 import java.text.SimpleDateFormat;
 import java.util.Calendar;
+import java.util.Date;
 
 public class AddHabitActivity extends AppCompatActivity {
     private AddHabitActivity activity = this;
@@ -17,9 +20,14 @@ public class AddHabitActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.add_habit);
 
+        final EditText habitTitle = (EditText) findViewById(R.id.habit_title_field);
+        final EditText habitReason = (EditText) findViewById(R.id.habit_reason_field);
+        final EditText dateSelector= (EditText) findViewById(R.id.habit_date_selector);
+        Button addHabitButton = (Button) findViewById(R.id.add_habit_button);
+
+
         // Date Picker (Source: https://goo.gl/nmN56M)
         final Calendar calendar = Calendar.getInstance();
-        final EditText dateSelector= (EditText) findViewById(R.id.habit_date_selector);
         final DatePickerDialog.OnDateSetListener date = new DatePickerDialog.OnDateSetListener() {
             @Override
             public void onDateSet(DatePicker view, int year, int monthOfYear,
@@ -44,6 +52,23 @@ public class AddHabitActivity extends AppCompatActivity {
                         calendar.get(Calendar.DAY_OF_MONTH)).show();
             }
         });
+
+        // submit button
+        addHabitButton.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View view){
+                String title = habitTitle.getText().toString();
+                String reason = habitReason.getText().toString();
+                Date d = calendar.getTime();
+
+                // TODO do error checking here
+
+                // TODO create a new habit object here and associate that with the user
+            }
+        });
+
+
+
     }
 
 }
