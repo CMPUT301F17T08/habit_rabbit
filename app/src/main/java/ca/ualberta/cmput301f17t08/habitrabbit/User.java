c41a67a7ca5c1938e6d2dfe4466fbb2f08063718
Zhipeng-Chang <zchang0302@gmail.com>
2017-11-02 20:16:40 -0600

    working on user class

--- app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/User.java
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/User.java
@@ -14,14 +14,28 @@ public class User {
     private ArrayList<User> followRequests;
     private ArrayList<Habit> historylist;
 
+    public User(String username){
+        this.username = username;
+        this.habitList = new ArrayList<Habit>();
+        this.followerList = new ArrayList<User>();
+        this.followingList = new ArrayList<User>();
+        this.followRequests = new ArrayList<User>();
+        this.historylist = new ArrayList<Habit>();
+
+    }
+
+    public void addFollower(User follower) { return; }
+    public void removeFollower(User follower) { return; }
+    public void addHabit(Habit habit) { return; }
+    public void removeHabit(Habit habit) { return; }
+    public ArrayList<Habit> filterHistory(String keyword, String filterType){return null;}
+    public ArrayList<Habit> habitmissed(Habit habit){return null;}
+
+
 
 
     public ArrayList<User> getFollowers() { return null; }
-    public void addFollower(User follower) { return; }
-    public void removeFollower(User follower) { return; }
 
     public ArrayList<Habit> getHabits() { return null; }
-    public void addHabit(Habit habit) { return; }
-    public void removeHabit(Habit habit) { return; }
 
 }
