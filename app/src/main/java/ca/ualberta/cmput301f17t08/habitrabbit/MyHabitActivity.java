a290ac2bee199bdde0cc92449fc8ac936d2f8c0f
Micah Seneshen <micah.seneshen@blizzardbird.com>
2017-11-09 16:21:28 -0700

    Added onClickListeners to menu and add habit button

--- app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/MyHabitActivity.java
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/MyHabitActivity.java
@@ -1,9 +1,12 @@
 package ca.ualberta.cmput301f17t08.habitrabbit;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.view.View;
+import android.widget.Button;
 
 import java.lang.reflect.Array;
 import java.util.ArrayList;
@@ -19,6 +22,8 @@ public class MyHabitActivity extends AppCompatActivity {
     private ArrayList<Habit> habitList;
     private HabitsAdapter cAdapt;
     private RecyclerView habitsRecyclerView;
+    private Button menuButton;
+    private Button addHabitButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -27,12 +32,32 @@ public class MyHabitActivity extends AppCompatActivity {
 
         habitList = LoginManager.getInstance().getCurrentUser().getHabits();
 
+        menuButton = (Button) findViewById(R.id.menu_button);
+        addHabitButton = (Button) findViewById(R.id.add_habit_button);
         habitsRecyclerView = (RecyclerView) findViewById(R.id.habit_recyclerview);
         habitsRecyclerView.setLayoutManager(new LinearLayoutManager(getApplicationContext(), LinearLayoutManager.VERTICAL, false));
 
         cAdapt = new HabitsAdapter(habitList, this);
         habitsRecyclerView.setAdapter(cAdapt);
 
+        menuButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity, MenuActivity.class);
+                startActivity(intent);
+            }
+        });
+
+        addHabitButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(activity, AddHabitActivity.class);
+                startActivity(intent);
+            }
+        });
+
     }
+
+
 
 }
