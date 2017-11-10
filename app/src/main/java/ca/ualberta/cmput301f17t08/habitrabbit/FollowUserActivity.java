167a30b69a26aa00c3a168f8e3eb2228ba81722b
Maharsh Patel <patel@maharsh.net>
2017-11-09 20:39:19 -0700

    started follow user activity

---
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/FollowUserActivity.java
@@ -0,0 +1,32 @@
+package ca.ualberta.cmput301f17t08.habitrabbit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+public class FollowUserActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.follow_user);
+
+        final EditText usernameField = findViewById(R.id.username_input_field);
+        Button addButton = findViewById(R.id.follow_user_button);
+
+        addButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String username = usernameField.getText().toString();
+
+                if (username.length() > 0){
+                    // TODO get the user associated with username from the database and add current user's name to their pending list
+                    return;
+                }
+            }
+        });
+    }
+
+}
