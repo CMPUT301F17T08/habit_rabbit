ca9dabb579a4f61c0d198c260b03b4eaa42eba95
Yuxuan Zhao <yzhao9@ualberta.ca>
2017-11-21 14:10:35 -0700

    feedactivity is done

---
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/FeedActivity.java
@@ -0,0 +1,12 @@
+package ca.ualberta.cmput301f17t08.habitrabbit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class FeedActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+}
