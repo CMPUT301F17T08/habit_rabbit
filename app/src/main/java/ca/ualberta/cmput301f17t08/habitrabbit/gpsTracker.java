7c0b52476d8ba37d5f7e8c47959463ee2e071bd5
cjlee <cjlee@ualberta.ca>
2017-11-21 22:49:46 -0700

    map edits still in progress

--- app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/gpsTracker.java
+++ app/src/main/java/ca/ualberta/cmput301f17t08/habitrabbit/gpsTracker.java
@@ -1,13 +1,18 @@
 package ca.ualberta.cmput301f17t08.habitrabbit;
 
+import android.app.Activity;
 import android.app.Service;
 import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.location.Location;
 import android.location.LocationListener;
 import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.util.Log;
 
 public class gpsTracker extends Service implements LocationListener {
 
@@ -22,16 +27,34 @@ public class gpsTracker extends Service implements LocationListener {
                 .getSystemService(LOCATION_SERVICE);
     }
 
-    public Location getLocation(String provider) {
-        if (locationManager.isProviderEnabled(provider)) {
-            locationManager.requestLocationUpdates(provider,
-                    MIN_TIME_FOR_UPDATE, MIN_DISTANCE_FOR_UPDATE, this);
-            if (locationManager != null) {
-                location = locationManager.getLastKnownLocation(provider);
-                return location;
-            }
+    /**
+     * Checks permissions and gets the current location. The current location is stored
+     * as latitude and longitude double values in the main model class.
+     */
+    public void getCurrentLocation() {
+        // Get the current location.
+        // http://stackoverflow.com/questions/32491960/android-check-permission-for-locationmanager
+        // Check if we have proper permissions to get the coarse lastKnownLocation.
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.
+                ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+
+            Log.i("HABBIT RABBIT", "Requesting coarse permission.");
+            // Request the permission.
+            // Dummy request code 8 used.
+            ActivityCompat.requestPermissions(context,
+                    new String[] {android.Manifest.permission.ACCESS_COARSE_LOCATION}, 8);
         }
-        return null;
+
+        // Check if we have proper permissions to get the fine lastKnownLocation.
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.
+                ACCESS_FINE_LOCATION ) != PackageManager.PERMISSION_GRANTED ) {
+
+            Log.i("debugMaps","Requesting fine permission");
+            // Request the permission.
+            // Dummy request code 8 used.
+            ActivityCompat.requestPermissions(this,
+                    new String[] {android.Manifest.permission.ACCESS_FINE_LOCATION}, 8);
+        }
     }
 
     @Override
